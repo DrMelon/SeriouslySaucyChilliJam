66851bb914e3d74586bf10ab5d7a664509d96ff4
johnjoemcbob <mcormack1994@hotmail.co.uk>
2015-04-19 14:10:06 +0100

    Scoreboard fixed

--- ChilliJam/AppSource/State_Score.cpp
+++ ChilliJam/AppSource/State_Score.cpp
@@ -47,7 +47,7 @@
 
 namespace ChilliJam
 {
-	
+
 	void State_Score::CreateSystems()
 	{
 		//Add systems here.
@@ -57,68 +57,68 @@ namespace ChilliJam
 	{
 		//Create the camera component
 		CSRendering::RenderComponentFactory* renderComponentFactory = CSCore::Application::Get()->GetSystem<CSRendering::RenderComponentFactory>();
-		CSRendering::CameraComponentSPtr cameraComponent = renderComponentFactory->CreatePerspectiveCameraComponent(CSCore::MathUtils::k_pi / 2.0f, 1.0f, 100.0f);
+		CSRendering::CameraComponentSPtr cameraComponent = renderComponentFactory->CreatePerspectiveCameraComponent( CSCore::MathUtils::k_pi / 2.0f, 1.0f, 100.0f );
 
 		//create the camera entity and add the camera component
 		CSCore::EntitySPtr cameraEntity = CSCore::Entity::Create();
-		cameraEntity->AddComponent(cameraComponent);
+		cameraEntity->AddComponent( cameraComponent );
 
 		//add the camera to the scene
-		GetScene()->Add(cameraEntity);
+		GetScene()->Add( cameraEntity );
 
 		string text = "score";
-			
+
 
 		// Get a reference to the resource pool for this application
 		auto resourcepool = CSCore::Application::Get()->GetResourcePool();
 		auto widgetfactory = CSCore::Application::Get()->GetWidgetFactory();
 
 		// Load ui widget
-		auto templatewidget = resourcepool->LoadResource<CSUI::WidgetTemplate>(CSCore::StorageLocation::k_package, "UI/scoreBoard.csui");
-			
-		scoreboard = widgetfactory->Create(templatewidget);
-		
-		GetUICanvas()->AddWidget(scoreboard);
+		auto templatewidget = resourcepool->LoadResource<CSUI::WidgetTemplate>( CSCore::StorageLocation::k_package, "UI/scoreBoard.csui" );
 
-	//	scoreboard->GetWidget("PlayButton")->GetWidget("PlayButtonLabel")->GetComponent<CSUI::TextComponent>()->SetText(text);
+		scoreboard = widgetfactory->Create( templatewidget );
+
+		GetUICanvas()->AddWidget( scoreboard );
+
+		//	scoreboard->GetWidget("PlayButton")->GetWidget("PlayButtonLabel")->GetComponent<CSUI::TextComponent>()->SetText(text);
 		//Test
 
-		UI_moneyValue = scoreController.getPrevTotalMoney();
-		UI_juiceValue = scoreController.getPrevTotalJuice();
+		UI_moneyValue = 0;
+		UI_juiceValue = 0;
 
 		ButtonConnection = scoreboard->GetWidget( "PlayButton" )->GetReleasedInsideEvent().OpenConnection(
 			[]( CSUI::Widget* in_widget, const CSInput::Pointer& in_pointer, CSInput::Pointer::InputType in_inputType )
-			{
-				App* application = (App*) CSCore::Application::Get();
-				State_Score* state = (State_Score*) application->GetStateManager()->GetActiveState().get();
-				state->Continue();
-			}
+		{
+			App* application = (App*) CSCore::Application::Get();
+			State_Score* state = (State_Score*) application->GetStateManager()->GetActiveState().get();
+			state->Continue();
+		}
 		);
 	}
 
-	void State_Score::OnUpdate(f32 in_deltaTime)
+	void State_Score::OnUpdate( f32 in_deltaTime )
 	{
 		int money_Difference = scoreController.getTotalMoney() - scoreController.getPrevTotalMoney();
-		if (UI_moneyValue != money_Difference)
+		if ( UI_moneyValue != money_Difference )
 		{
 			UI_moneyValue += 1;
-			std::string moneyText = std::to_string(UI_moneyValue);
-			scoreboard->GetWidget("MoneyScore")->GetComponent<CSUI::TextComponent>()->SetText(moneyText);
+			std::string moneyText = std::to_string( scoreController.getPrevTotalMoney() + UI_moneyValue );
+			scoreboard->GetWidget( "MoneyScore" )->GetComponent<CSUI::TextComponent>()->SetText( moneyText );
 		}
 		int juice_Difference = scoreController.getTotalJuice() - scoreController.getPrevTotalJuice();
-		if (UI_juiceValue != juice_Difference)
+		if ( UI_juiceValue != juice_Difference )
 		{
 			UI_juiceValue += 1;
-			std::string juiceText = std::to_string(UI_juiceValue);
-			scoreboard->GetWidget("AlienScore")->GetComponent<CSUI::TextComponent>()->SetText(juiceText);
+			std::string juiceText = std::to_string( scoreController.getPrevTotalJuice() + UI_juiceValue );
+			scoreboard->GetWidget( "AlienScore" )->GetComponent<CSUI::TextComponent>()->SetText( juiceText );
 		}
 
-		std::string moneyGained = std::to_string(money_Difference);
-		std::string juiceGained = std::to_string(juice_Difference);
+		std::string moneyGained = std::to_string( money_Difference );
+		std::string juiceGained = std::to_string( juice_Difference );
 
-		scoreboard->GetWidget("MoneyUpdate")->GetComponent<CSUI::TextComponent>()->SetText(moneyGained);
-		scoreboard->GetWidget("AlienUpdate")->GetComponent<CSUI::TextComponent>()->SetText(juiceGained);
-		
+		scoreboard->GetWidget( "MoneyUpdate" )->GetComponent<CSUI::TextComponent>()->SetText( moneyGained );
+		scoreboard->GetWidget( "AlienUpdate" )->GetComponent<CSUI::TextComponent>()->SetText( juiceGained );
+
 	}
 
 	void State_Score::OnDestroy()
@@ -129,7 +129,7 @@ namespace ChilliJam
 	void State_Score::Continue()
 	{
 		App* application = (App*) CSCore::Application::Get();
-		application->GetStateManager()->Change( (CSCore::StateSPtr) new State_DayBegin );
+		application->GetStateManager()->Change( ( CSCore::StateSPtr ) new State_DayBegin );
 	}
 
 	CSUI::WidgetSPtr CreateScoreScreen()
@@ -137,9 +137,9 @@ namespace ChilliJam
 		auto widgetFactory = CSCore::Application::Get()->GetWidgetFactory();
 		auto resPool = CSCore::Application::Get()->GetResourcePool();
 
-		auto scoreWidget = resPool->LoadResource<CSUI::WidgetTemplate>(CSCore::StorageLocation::k_package, "UI/scoreBoard.csui");
+		auto scoreWidget = resPool->LoadResource<CSUI::WidgetTemplate>( CSCore::StorageLocation::k_package, "UI/scoreBoard.csui" );
 
-		CSUI::WidgetSPtr widget = widgetFactory->Create(scoreWidget);
+		CSUI::WidgetSPtr widget = widgetFactory->Create( scoreWidget );
 
 		return widget;
 	}
